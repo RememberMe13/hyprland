1aa99f2cca9fd3e7f302ace5d3181719a3d8208d
Henry <emeraldwolf356@gmail.com>
2026-08-24 18:50:16 +1000

    removed all hard coded links

--- .config/waybar/scripts/wall.sh
+++ .config/waybar/scripts/wall.sh
@@ -4,7 +4,7 @@
 file="$(find ~/Pictures/lowerResolution/ -type f | awk -F/ '{print $NF "\t" $0}' | fuzzel -d --with-nth=1 --accept-nth=2 -a top-right -l 14 -w 23 -p "Search: ")"
 
 if [[ -n "$file" ]]; then
-	ln -sf "$file" /home/henry/.config/hypr/symlink
+	ln -sf "$file" ~/.config/hypr/symlink
     awww img "$file" --transition-type grow --transition-fps 60 --transition-duration 2.8
     notify-send BEFORE
     matugen image "$file" --source-color-index 0
--- .config/waybar/scripts/wallpaper.sh
+++ .config/waybar/scripts/wallpaper.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 # This is the random wallpaper chooser, that fires when user right clicks on waybar image
-file="$(find /home/henry/Pictures/lowerResolution/ -type f | shuf -n 3 | shuf -n 1)"
-ln -sf "$file" /home/henry/.config/hypr/symlink
+file="$(find ~/Pictures/lowerResolution/ -type f | shuf -n 3 | shuf -n 1)"
+ln -sf "$file" ~/.config/hypr/symlink
 awww img "$file" --transition-type grow --transition-fps 60 --transition-duration 2.8
 notify-send BEFORE
 matugen image "$file" --source-color-index 0
