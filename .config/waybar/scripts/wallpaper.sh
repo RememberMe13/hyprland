0c71828285634cce7c8875299c7b03c388028115
Henry <emeraldwolf356@gmail.com>
2026-09-03 09:06:00 +1000

    '

--- .config/waybar/scripts/wallpaper.sh
+++ .config/waybar/scripts/wallpaper.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # This is the random wallpaper chooser, that fires when user right clicks on waybar image
-file="$(find ~/Pictures/lowerResolution/ -type f | shuf -n 3 | shuf -n 1)"
+file="$(find ~/Pictures/lowerResolution/ -type f | shuf -n 10 | shuf -n 1)"
 ln -sf "$file" ~/.config/hypr/symlink
 awww img "$file" --transition-type grow --transition-fps 60 --transition-duration 2.8
 notify-send BEFORE
